5b3d151d62d96ff1b4ec04b34992683807c82d5f
Lukasz Kowalik <kowalix007@gmail.com>
2021-01-26 20:29:35 +0000

    start with quiz section

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,10 @@
 console.log("JS works!");
+
+const startButton = document.getElementById("startButton");
+const welcomePage = document.getElementById("welcomePage");
+const questionsPage = document.getElementById("questionsPage");
+
+startButton.addEventListener("click", ()=>{
+  welcomePage.classList.add("hidden");
+  questionsPage.classList.remove("hidden");
+}); 
