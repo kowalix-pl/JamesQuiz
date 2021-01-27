b636d62dcae2de2d32d786dd2d82d2ad2f4a5138
Lukasz Kowalik <kowalix007@gmail.com>
2021-01-27 19:09:12 +0000

    create radio buttons

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,30 @@
-console.log("JS works!");
 
 const startButton = document.getElementById("startButton");
 const welcomePage = document.getElementById("welcomePage");
 const questionsPage = document.getElementById("questionsPage");
+const quizRadioButtons = document.getElementById("quizInput");
 
 startButton.addEventListener("click", ()=>{
   welcomePage.classList.add("hidden");
   questionsPage.classList.remove("hidden");
 }); 
+
+/* <input type="radio" id="jsradio" name="quiztype" value="javascript">
+<label for="jsradio">JavaScript</label><br> */
+
+function radioButton(label,value) {
+  const newInputId = `${value}radio`;
+  const newInput = document.createElement("input");
+  newInput.setAttribute("type","radio");
+  newInput.setAttribute("name","quiztype");
+  newInput.setAttribute("value",value);
+  newInput.setAttribute("id", newInputId);
+  quizRadioButtons.append(newInput);
+  const newLabel = document.createElement("label");
+  newLabel.setAttribute("for",newInputId);
+  newLabel.textContent = label;
+  quizRadioButtons.append(newLabel);
+};
+
+radioButton("Python","python");
+radioButton("Ruby","ruby");
