9a8352a7300ccd49d5944dadc1770cfdbe927bd4
Lukasz Kowalik <kowalix007@gmail.com>
2021-02-02 18:03:59 +0000

    create page class

--- frontend/htmlPageElements.js
+++ frontend/htmlPageElements.js
@@ -1,8 +1,6 @@
 // html elements
 const htmlElement = {};
 htmlElement.startButton = document.getElementById("startButton");
-htmlElement.welcomePage = document.getElementById("welcomePage");
-htmlElement.questionsPage = document.getElementById("questionsPage");
 htmlElement.quizRadioButtons = document.getElementById("quizInput");
 htmlElement.usernameInput = document.getElementById("usernameInput");
 htmlElement.questionsContainer = document.getElementById("questionsContainer");
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,18 @@
+//Class that shows and hides the page
+class Page{
+  constructor(containerID){
+   this._element = document.getElementById(containerID);
+  }
+  hide(){
+    this._element.classList.add("hidden");
+  }
+  show(){
+    this._element.classList.remove("hidden");
+  }
+}
+const welcomePage = new Page("welcomePage");
+const questionsPage = new Page("questionsPage");
+
 function radioButtonForWelcomePage(label,value) {
   const newInputId = `${value}radio`;
   htmlCreator.radio(htmlElement.quizRadioButtons,{id:newInputId,label:label,value:value,name:"quiztype"});
@@ -21,8 +36,8 @@ async function startQuiz(username,quizName){
 };
 
 htmlElement.startButton.addEventListener("click", ()=>{
-  htmlElement.welcomePage.classList.add("hidden");
-  htmlElement.questionsPage.classList.remove("hidden");
+  welcomePage.hide();
+  questionsPage.show();
   const quizName = document.querySelector('input[name="quiztype"]:checked').value;
   startQuiz(htmlElement.usernameInput.value, quizName);
 }); 
