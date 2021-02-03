c53c545f0df6ca4fe8e7528459a8bfe8759fb621
Lukasz Kowalik <kowalix007@gmail.com>
2021-02-03 18:53:57 +0000

    initialize quiz controller

--- frontend/script.js
+++ frontend/script.js
@@ -1,34 +1,45 @@
-const welcomePage = new WelcomePage("welcomePage");
-const questionsPage = new QuestionsPage("questionsPage");
 
-(async ()=>{
-  const quizNames = await backendAPI.getQuizNames();
-  welcomePage.displayQuizForm(quizNames);
-  //Handles the click on the start button
-  welcomePage.startButtonClicked((userName,quizName)=>{
-    welcomePage.hide();
-    questionsPage.show();
-    startQuiz(userName, quizName);
-  })
+class QuizController {
+  constructor(){
+    this.welcomePage = new WelcomePage("welcomePage");
+    this.questionsPage = new QuestionsPage("questionsPage");
+  }
+  
+  async welcome(){
+    const quizNames = await backendAPI.getQuizNames();
+    this.welcomePage.displayQuizForm(quizNames);
+    //Handles the click on the start button
+    this.welcomePage.startButtonClicked(async (userName,quizName)=>{
+      this.welcomePage.hide();
+      this.questionsPage.show();
+      await this.startQuiz(userName, quizName);
+    })
+  }
+  async run(){
+    await this.welcome();
+  }
+  async startQuiz(username,quizName){
+    const ids = await backendAPI.getQuizQuestionsList(quizName);
+    let currentQuestionIndex = 0;
+    await this._question(ids[currentQuestionIndex],currentQuestionIndex+1);
+    this.questionsPage.onSubmitAnswer(async (answer)=>{
+      console.log(`Submitted answer for ${currentQuestionIndex}, answer is:${answer}`);
+      currentQuestionIndex++;
+      if (currentQuestionIndex < ids.length){
+        await this._question(ids[currentQuestionIndex],currentQuestionIndex+1);
+      }else{
+       console.log("quiz is finished");
+      };
+    }); 
+  }
+  //function to get the data from the backend and display it
+  async _question(id,questionNumber){
+    const questionData = await backendAPI.getQuestion(id);
+    this.questionsPage.displayQuestion(questionNumber,questionData.text,questionData.answers);
+  }
+}
+const quizController = new QuizController();
+(async()=>{
+   await quizController.run();
 })();
 
-async function startQuiz(username,quizName){
-  const ids = await backendAPI.getQuizQuestionsList(quizName);
-  let currentQuestionIndex = 0;
-  await question(ids[currentQuestionIndex],currentQuestionIndex+1);
-  questionsPage.onSubmitAnswer(async (answer)=>{
-    console.log(`Submitted answer for ${currentQuestionIndex}, answer is:${answer}`);
-    currentQuestionIndex++;
-    if (currentQuestionIndex < ids.length){
-      await question(ids[currentQuestionIndex],currentQuestionIndex+1);
-    }else{
-     console.log("quiz is finished");
-    };
-  }); 
-};
-
-//function to get the data from the backend and display it
-async function question(id,questionNumber){
-  const questionData = await backendAPI.getQuestion(id);
-  questionsPage.displayQuestion(questionNumber,questionData.text,questionData.answers);
-};
