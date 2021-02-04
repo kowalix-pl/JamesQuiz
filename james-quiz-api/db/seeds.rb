82431ff5448255940389393bbbefa9185b1a245b
Lukasz Kowalik <kowalix007@gmail.com>
2021-02-04 17:51:46 +0000

    generate question seed

--- james-quiz-api/db/seeds.rb
+++ james-quiz-api/db/seeds.rb
@@ -1,4 +1,30 @@
 
- java_script_quiz = Quiz.find_or_create_by({ name: 'JavaScript'})
- html_quiz = Quiz.find_or_create_by({ name: 'HTML'})
- css_quiz = Quiz.find_or_create_by({ name: 'CSS'})
+ java_script_quiz = Quiz.find_by({ name: 'JavaScript'})
+ if java_script_quiz.nil? 
+    java_script_quiz=Quiz.create(name:"JavaScript")
+    java_script_quiz.questions.create({
+      text: "How do you do?",
+      answer: "Great",
+      choices: ["Not so good","Great","Awesome"]
+    })
+    java_script_quiz.questions.create({
+      text: "What day is today?",
+      answer: "Friday",
+      choices: ["Monday","Tuesday","Friday"]
+    })
+ end 
+
+ html_quiz = Quiz.find_by({ name: 'HTML'})
+ if html_quiz.nil? 
+    html_quiz=Quiz.create(name:"HTML")
+    html_quiz.questions.create({
+      text: "Are you married?",
+      answer: "yes",
+      choices: ["yes","no","do not want to say"]
+    })
+    html_quiz.questions.create({
+      text: "How old are you?",
+      answer: "5",
+      choices: ["5","10","15"]
+    })
+ end 
